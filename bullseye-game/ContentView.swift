4dd778a3a06ba7fa0b07503d3d18285423fedc32
urosbmx <urosbmx@gmail.com>
2022-03-07 20:26:29 +0100

    first commit

---
+++ bullseye-game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bullseye-game
+//
+//  Created by Uroš Katanić on 7.3.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bullseye-game/bullseye_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseye_gameApp.swift
+//  bullseye-game
+//
+//  Created by Uroš Katanić on 7.3.22..
+//
+
+import SwiftUI
+
+@main
+struct bullseye_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
