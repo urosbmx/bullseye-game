1fe16e9a658a975b33b1935ae8e6e5468847d7d6
urosbmx <urosbmx@gmail.com>
2022-03-07 21:15:16 +0100

    Copy of app

--- bullseye-game/ContentView.swift
+++ bullseye-game/ContentView.swift
@@ -6,14 +6,34 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var sliderValie:Double = 50.0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Text("🎯🎯🎯🎯\nPut the bullseys as close as you can to".uppercased())
+                    .font(.subheadline)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom)
+            }
+            HStack{
+                Text("50")
+                    .font(.title)
+                    .bold()
+            }
+            HStack{
+                    Text("1")
+                    .bold()
+                Slider(value: self.$sliderValie, in: 1.0...100.0)
+                    Text("100")
+                    .bold()
+            }
+            .padding(.horizontal)
+            
+        }
+        
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
